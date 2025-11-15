0f8a70f5291cd9645200e178946f133e2bd59a72
shossen1999 <shossen1999@gmail.com>
2025-11-15 20:15:24 +0600

    inserted module.exports

--- index.js
+++ index.js
@@ -714,12 +714,14 @@ app.get("/", (req, res) => {
   res.send("PawPalace server running");
 });
 
+app.get("/test", (req, res) => {
+  res.send("this is testing");
+});
 
-// if(process.env.NODE_ENV !== "production"){
-//   app.listen(port, () => {
-//   console.log(`Server running on port: ${port}`);
-// });
-// }
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(5000, () => console.log("Server running locally"));
+}
 
 
 
